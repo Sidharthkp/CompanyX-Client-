190a067df7f4b88326f4933ffd28c7cf9bbf04f0
Sidharthkp <sidharthkannan20@gmail.com>
2023-02-20 18:31:42 +0530

    added customers list in hr home

--- src/partials/dashboard/DashboardCard10.jsx
+++ src/partials/dashboard/DashboardCard10.jsx
@@ -1,55 +1,30 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 import Image01 from '../../images/user-36-05.jpg';
-import Image02 from '../../images/user-36-06.jpg';
-import Image03 from '../../images/user-36-07.jpg';
-import Image04 from '../../images/user-36-08.jpg';
-import Image05 from '../../images/user-36-09.jpg';
 
 function DashboardCard10() {
 
-  const customers = [
-    {
-      id: '0',
-      image: Image01,
-      name: 'Alex Shatov',
-      email: 'alexshatov@gmail.com',
-      location: '🇺🇸',
-      spent: '$2,890.66',
-    },
-    {
-      id: '1',
-      image: Image02,
-      name: 'Philip Harbach',
-      email: 'philip.h@gmail.com',
-      location: '🇩🇪',
-      spent: '$2,767.04',
-    },
-    {
-      id: '2',
-      image: Image03,
-      name: 'Mirko Fisuk',
-      email: 'mirkofisuk@gmail.com',
-      location: '🇫🇷',
-      spent: '$2,996.00',
-    },
-    {
-      id: '3',
-      image: Image04,
-      name: 'Olga Semklo',
-      email: 'olga.s@cool.design',
-      location: '🇮🇹',
-      spent: '$1,220.66',
-    },
-    {
-      id: '4',
-      image: Image05,
-      name: 'Burak Long',
-      email: 'longburak@gmail.com',
-      location: '🇬🇧',
-      spent: '$1,890.66',
-    },
-  ];
+  const [users, setUsers] = useState([]);
+
+  const getUsers = async () => {
+    const res = await axios.get("http://localhost:4111/hr/getUsers");
+    setUsers(res.data);
+  }
+
+  const customers = users
+    // {
+    //   id: '0',
+    //   image: Image01,
+    //   name: 'Alex Shatov',
+    //   email: 'alexshatov@gmail.com',
+    //   location: '🇺🇸',
+    //   spent: '$2,890.66',
+    // },
+
+  useEffect(() => {
+    getUsers();
+  }, [])
 
   return (
     <div className="col-span-full xl:col-span-6 bg-white shadow-lg rounded-sm border border-slate-200">
@@ -83,7 +58,7 @@ function DashboardCard10() {
               {
                 customers.map(customer => {
                   return (
-                    <tr key={customer.id}>
+                    <tr key={customer._id}>
                       <td className="p-2 whitespace-nowrap">
                         <div className="flex items-center">
                           <div className="w-10 h-10 shrink-0 mr-2 sm:mr-3">
